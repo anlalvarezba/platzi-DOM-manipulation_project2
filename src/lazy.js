aeaff4664860c6792af5d7df2d36c62966e777fd
anlalvarezba <anlavarezba@unal.edu.co>
2022-02-05 17:28:52 -0500

    Last changes made by the Platzi teacher. It hasn't the images shadow, the remove button nor the console notification.

---
+++ src/lazy.js
@@ -0,0 +1,21 @@
+const isIntersecting = (entry) => {
+    return entry.isIntersecting;
+}
+
+const loadImage = (entry) => {
+    const container = entry.target; 
+    const imagen = container.firstChild;
+    const url = imagen.dataset.src;
+
+    imagen.src = url;
+    observer.unobserve(container)
+}
+
+const observer = new IntersectionObserver((entries) => {
+    entries.filter(isIntersecting).forEach(loadImage);
+});
+
+export const registerImage = (image) => {
+    //IntersectionObserver -> observer(image);
+    observer.observe(image);
+}
